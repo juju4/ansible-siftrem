de4d219c2c7635de82f58b94008306f70324cdb2
juju4 <juju4@users.noreply.github.com>
2016-11-27 20:55:24 -0500

    missing role deps: docker

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
+[ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift $rolesdir/juju4.gift
 [ ! -d $rolesdir/juju4.sift ] && git clone https://github.com/juju4/ansible-sift $rolesdir/juju4.sift
 [ ! -d $rolesdir/juju4.remnux ] && git clone https://github.com/juju4/ansible-remnux $rolesdir/juju4.remnux
