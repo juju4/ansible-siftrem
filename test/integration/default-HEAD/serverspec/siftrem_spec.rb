964cf299641644a33e6d7b844584e5e15b0d0d3f
juju4 <juju4@users.noreply.github.com>
2019-07-13 07:45:44 -0400

    add test suite for HEAD versions

---
+++ test/integration/default-HEAD/serverspec/siftrem_spec.rb
@@ -0,0 +1,44 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe service('snort') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('argus') do
+#  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('samba') do
+#  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(22) do
+  it { should be_listening }
+end
+
+describe port(445) do
+  it { should be_listening }
+end
+
+describe file('/usr/share/remnux') do
+  it { should be_directory }
+end
+
+describe file('/home/sansforensics/Desktop/REMnux Tools Sheet') do
+  it { should be_symlink }
+end
+
+describe file('/home/sansforensics/Desktop/sift-cheatsheet.pdf') do
+  it { should be_symlink }
+end
+
+describe command('xvfb-run --server-args="-screen 0, 1024x768x24" cutycapt --url="http://www.google.com" --out="/tmp/google.png"') do
+  its(:exit_status) { should eq 0 }
+end
+
